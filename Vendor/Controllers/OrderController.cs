ce60a8b93e9c192bbb2debd4e5be397ea6bfe406
Jake Edgar <jakeedgar1012@gmail.com>
2022-05-13 15:42:02 -0700

    update vendor controller and add boilerplate order controller logic

---
+++ Vendor/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using BakeryVendor.Models;
+using System.Collections.Generic;
+
+namespace BakeryVendor.Controllers
+{
+  public class OrdersController : Controller
+  {
+
+  }
+}
