6df57f31083143590ab26b2cccaca76eb06bd5c3
Jake Edgar <jakeedgar1012@gmail.com>
2022-05-13 09:35:28 -0700

    update file structure and add boilerplate code

---
+++ Vendor/Models/Order.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Vendor.Models
+{
+  public class Order
+  {
+    public Order()
+    {
+      
+    }
+  }
+}
---
+++ Vendor/Models/Vendor.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Vendor.Models 
+{
+  public class Vendor 
+  {
+    public Vendor()
+    {
+
+    }
+  }
+}
